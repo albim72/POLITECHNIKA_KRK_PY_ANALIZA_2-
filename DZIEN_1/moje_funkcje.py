8016f7f85fb27732ccf8a5c02b0e87aba7123c7a
albim72 <marcin.albiniak72@gmail.com>
2023-12-09 11:42:43 +0100

    Update moje_funkcje.py

--- DZIEN_1/moje_funkcje.py
+++ DZIEN_1/moje_funkcje.py
@@ -13,3 +13,12 @@ print(policz(3,7,8,2))
 print(policz(1.11,87.3,-11.2,0.1))
 
 print(n)
+
+#przykład 2
+
+def rank(*lang,nrrank,**inne):
+    print(f"rankig języków programowania nr {nrrank}: pierwsze miejsca: {lang[0]}, "
+          f"drugie miejsce: {lang[1]}, trzecie miejsce: {lang[2]}")
+
+rank("Python","Java","C++",nrrank=56)
+rank("Python","C#","Java","C++","JavaScript",nrrank=77,ver="5.5")
