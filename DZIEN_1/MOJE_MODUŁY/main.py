4296b27d492902ab0b280781ed04c989cab3c7b1
albim72 <marcin.albiniak72@gmail.com>
2023-12-09 13:56:55 +0100

    Create main.py

---
+++ DZIEN_1/MOJE_MODUŁY/main.py
@@ -0,0 +1,8 @@
+#import dane
+#import dane as dn
+from dane import ksiegarnia as filia
+from dane import book as bk
+
+print("_____________________ czytanie bezpośrednie _____________________")
+print(filia)
+print(bk)
