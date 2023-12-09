133b4f9999ef58828cf3ec8ad28609594674d35a
albim72 <marcin.albiniak72@gmail.com>
2023-12-09 14:11:25 +0100

    Update main.py

--- DZIEN_1/MOJE_MODUŁY/main.py
+++ DZIEN_1/MOJE_MODUŁY/main.py
@@ -2,7 +2,18 @@
 #import dane as dn
 from dane import ksiegarnia as filia
 from dane import book as bk
+from funkcje.bfunkcje import czytaj_liste,czytaj_slownik
+from klasy.cklasa import CDane
 
 print("_____________________ czytanie bezpośrednie _____________________")
 print(filia)
 print(bk)
+
+print("_____________________ czytanie za pomocą funkcji _____________________")
+czytaj_liste(filia)
+czytaj_slownik(bk)
+
+print("_____________________ czytanie za pomocą obiektu _____________________")
+cd = CDane(filia,bk)
+cd.czytajl()
+cd.czytajs()
