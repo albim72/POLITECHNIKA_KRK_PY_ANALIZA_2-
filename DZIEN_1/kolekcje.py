8ca8e6ae8dafd2eff251ba419b974bb1719a1a28
albim72 <marcin.albiniak72@gmail.com>
2023-12-09 11:05:10 +0100

    Create kolekcje.py

---
+++ DZIEN_1/kolekcje.py
@@ -0,0 +1,45 @@
+print("Kolekcje języka Python")
+
+liczby = [3,56,78,24,-45,1345,2.2,453,12,-56,0,789,0.33,1.2,456,-111,99,23]
+print(liczby)
+print(liczby[0])
+#CTRL+D  - powielenie linii/bloku
+#CTRL +/ - komentowanie/odkomentowanie linii/bloku
+print(liczby[1])
+print(liczby[2:7])
+print(liczby[-1])
+print(liczby[6:])
+print(liczby[:8])
+
+liczby.append(78)
+print(liczby)
+
+liczby.insert(4,101)
+print(liczby)
+
+liczby.remove(1345)
+print(liczby)
+
+del liczby[0]
+print(liczby)
+
+print(sorted(liczby,reverse=True))
+print(liczby)
+
+liczby.sort()
+print(liczby)
+
+nb = [4,4,4,4,4,1]
+
+liczby = liczby + nb #konkatenacja list (połączenie)
+
+print(liczby)
+print(type(liczby))
+
+#krotka (tuple)
+
+miasta = ("Kraków","Lublin","Warszawa","Poznań","Wrocław","Kraków","Gdańsk")
+print(miasta)
+print(type(miasta))
+print(miasta[2:5])
+print(miasta.count("Kraków"))
