dea4cda001a64d87dc162f4d1d2d508de9f8a5c7
albim72 <marcin.albiniak72@gmail.com>
2023-12-09 11:57:23 +0100

    Update moje_funkcje.py

--- DZIEN_1/moje_funkcje.py
+++ DZIEN_1/moje_funkcje.py
@@ -22,3 +22,32 @@ def rank(*lang,nrrank,**inne):
 
 rank("Python","Java","C++",nrrank=56)
 rank("Python","C#","Java","C++","JavaScript",nrrank=77,ver="5.5")
+
+#funkcje anonimowe
+print((lambda d:d+122)(10))
+print((lambda d,f:(d+122)/f)(10,2))
+
+b = lambda a,b,c=1:(a+b)/c
+
+print(b(1,2,6))
+print(b(1,2,))
+
+def multi(n):
+    return lambda a:a*n
+
+print(multi(5)(9))
+
+#zastosowania funkcji lambda
+num = [5,7,13,0,-45,13,47,11,0,-345,57,0,-.5,6.,118,-562,899]
+
+nbparz  = list(filter(lambda x:x%2==0,num))
+print(nbparz)
+
+cube = list(map(lambda x:x**3,num))
+print(cube)
+
+def dodaj(x):
+    return x+9
+
+dziewiec = list(map(dodaj,num))
+print(dziewiec)
