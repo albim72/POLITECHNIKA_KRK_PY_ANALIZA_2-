e56f5e2622a286d645a1f5c66601185b8088313c
albim72 <marcin.albiniak72@gmail.com>
2023-12-09 13:53:30 +0100

    Create dane.py

---
+++ DZIEN_1/MOJE_MODUŁY/dane.py
@@ -0,0 +1,9 @@
+ksiegarnia = [4,7,9,11,14,48,68,90]
+
+book = {
+    "tytuł":"Poradnik kulturysty",
+    "autor":"Andrzej Kowal",
+    "cena":49.99,
+    "liczba stron":390,
+    "nr filii":ksiegarnia
+}
